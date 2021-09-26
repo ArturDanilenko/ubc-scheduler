d6e3c500590c5edbfc348c603d9797c2a5388a56
Artur <danartur99@gmail.com>
2021-09-25 23:42:54 -0700

    Add backend and DB boilerplate and connect to a DB
    BAC-1
    BAC-2
    BAC-3

---
+++ backend/ubc-scheduler/src/test/java/com/ubcscheduler/ubcscheduler/UbcSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubcscheduler.ubcscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbcSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
