f2d20f364ce3e2df96170658f99ea011a51f848f
Artur <danartur99@gmail.com>
2021-11-24 00:12:56 -0800

    Remove unused boiler plate
    Add mappings
    Init scraper and some scraper utils

--- backend/ubc-scheduler/src/main/java/com/ubcscheduler/ubcscheduler/repository/CourseRepository.java
+++ backend/ubc-scheduler/src/main/java/com/ubcscheduler/ubcscheduler/repository/CourseRepository.java
@@ -4,6 +4,15 @@ import com.ubcscheduler.ubcscheduler.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByCourseId(String courseId);
+    boolean existsByCourseCode(String courseCode);
+    boolean existsByCourseCodeAndYear(String courseCode, short year);
+    Course findByCourseId(String courseId);
+    List<Course> findAllByCourseCode(String courseCode);
+    List<Course> findAllByCourseCodeAndYear(String courseCode, short year);
+
 }
